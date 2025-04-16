5f81b7a8d3e7cba5fb5428ed666db94b6fd8363a
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:16:07 +0300

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require('discord.js');
+const { getUser } = require('../database');
+
+module.exports = {
+  name: 'balance',
+  description: 'Check your balance.',
+  aliases: ['bal'],
+  async prefixExecute(message) {
+    const user = getUser(message.author.id);
+    const embed = new EmbedBuilder()
+      .setTitle('Your Wallet')
+      .setColor('#9a46ca')
+      .setDescription(`Wallet: ${user.wallet} Synths\nBank: ${user.bank} Synths`);
+
+    message.reply({ embeds: [embed] });
+  }
+};
