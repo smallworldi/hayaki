34221f6ec48e0c07bdce9cc7265447ee76638b0e
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 10:27:17 +0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,6 +1,13 @@
 const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const { getUser } = require('../database');
-const { createCanvas } = require('canvas');
+const { createCanvas, registerFont } = require('canvas');
+const path = require('path');
+
+
+registerFont(path.join(__dirname, '../assets/fonts/OpenSans-Bold.ttf'), {
+  family: 'Open Sans',
+  weight: 'bold'
+});
 
 module.exports = {
   name: 'balance',
@@ -12,23 +19,23 @@ module.exports = {
     const canvas = createCanvas(700, 300);
     const ctx = canvas.getContext('2d');
 
-    // Fundo
+
     ctx.fillStyle = '#2b2d31';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Texto
     ctx.fillStyle = '#ffffff';
-    ctx.font = '30px sans-serif';
+    ctx.font = '30px "Open Sans"';
     ctx.fillText(`Wallet: ${user.wallet} Synths`, 50, 100);
     ctx.fillText(`Bank: ${user.bank} Synths`, 50, 150);
 
-    // Gerar o buffer da imagem
+
     const buffer = canvas.toBuffer('image/png');
 
-    // Criar o anexo
+
     const attachment = new AttachmentBuilder(buffer, { name: 'balance-card.png' });
 
-    // Criar embed com imagem
+
     const embed = new EmbedBuilder()
       .setColor('#9a46ca')
       .setTitle('Your Balance')
@@ -36,7 +43,7 @@ module.exports = {
       .setImage('attachment://balance-card.png')
       .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL() });
 
-    // Enviar resposta
+
     message.reply({ embeds: [embed], files: [attachment] });
   }
 };
