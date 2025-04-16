c477fdcbe96977a60ae2f0563b6325349b91eb6a
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:18:39 +0300

    Create rob.js

---
+++ commands/rob.js
@@ -0,0 +1,49 @@
+const { EmbedBuilder } = require('discord.js');
+const { getUser, updateUser } = require('../database');
+
+module.exports = {
+  name: 'rob',
+  description: 'Attempt to rob another user.',
+  async prefixExecute(message, args) {
+    const target = message.mentions.members.first();
+    const thief = getUser(message.author.id);
+
+    if (!target || target.id === message.author.id || target.user.bot) {
+      return message.reply('Mention a valid user to rob.');
+    }
+
+    const victim = getUser(target.id);
+
+    if (victim.wallet < 100) {
+      return message.reply('That user has too little money to rob.');
+    }
+
+    const success = Math.random() < 0.5;
+
+    if (success) {
+      const amount = Math.floor(Math.random() * (victim.wallet * 0.3)) + 50;
+      thief.wallet += amount;
+      victim.wallet -= amount;
+      updateUser(message.author.id, thief);
+      updateUser(target.id, victim);
+
+      const embed = new EmbedBuilder()
+        .setColor('#9a46ca')
+        .setTitle('Robbery Successful')
+        .setDescription(`You stole ${amount} Synths from ${target.user.tag}.`);
+
+      message.reply({ embeds: [embed] });
+    } else {
+      const penalty = Math.floor(thief.wallet * 0.2);
+      thief.wallet -= penalty;
+      updateUser(message.author.id, thief);
+
+      const embed = new EmbedBuilder()
+        .setColor('#9a46ca')
+        .setTitle('You Got Caught')
+        .setDescription(`You failed the robbery and lost ${penalty} Synths.`);
+
+      message.reply({ embeds: [embed] });
+    }
+  }
+};
