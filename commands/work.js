a9b2be92a0cca7263cab2f4170f0cf2ba08f4071
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:38:03 +0300

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,28 +1,26 @@
 const { EmbedBuilder } = require('discord.js');
-const { getUser, updateUser } = require('../database');
+const { getBalance, updateBalance, getCooldown, setCooldown } = require('../database');
 
-const COOLDOWN_TIME = 3600000; // 1 hour in milliseconds
+const COOLDOWN_TIME = 3600000; // 1h
 
 module.exports = {
   name: 'work',
   description: 'Work to earn random Synths.',
   async prefixExecute(message) {
-    const user = getUser(message.author.id);
+    const userId = message.author.id;
     const now = Date.now();
 
-
-    if (now - user.lastWork < COOLDOWN_TIME) {
-      const timeLeft = COOLDOWN_TIME - (now - user.lastWork);
-      const minutesLeft = Math.ceil(timeLeft / 60000); // converte para minutos
+    const lastUsed = await getCooldown(userId, 'work');
+    if (now - lastUsed < COOLDOWN_TIME) {
+      const timeLeft = COOLDOWN_TIME - (now - lastUsed);
+      const minutesLeft = Math.ceil(timeLeft / 60000);
       return message.reply(`You can only work once per hour. Try again in ${minutesLeft} minute(s).`);
     }
 
-
     const amount = Math.floor(Math.random() * 400) + 100;
-    user.wallet += amount;
-    user.lastWork = now;
-    updateUser(message.author.id, user);
-
+    const currentBalance = await getBalance(userId);
+    await updateBalance(userId, currentBalance + amount);
+    await setCooldown(userId, 'work', now);
 
     const embed = new EmbedBuilder()
       .setColor('#9a46ca')
