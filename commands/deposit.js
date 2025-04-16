28f7effb16e2f1d1254d80f85c2d46dc95b654b6
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:59:01 +0300

    Update deposit.js

--- commands/deposit.js
+++ commands/deposit.js
@@ -1,27 +1,22 @@
 const { EmbedBuilder } = require('discord.js');
-const { getUserEconomy, updateUserEconomy } = require('../database');
+const { getUser, updateUser } = require('../database');
 
 module.exports = {
   name: 'deposit',
   description: 'Deposit Synths to your bank.',
   aliases: ['dep'],
   async prefixExecute(message, args) {
-    const userId = message.author.id;
-    const user = await getUserEconomy(userId);
-
-    if (!args[0]) {
-      return message.reply('Please specify an amount to deposit.');
-    }
-
-    const amount = args[0].toLowerCase() === 'all' ? user.wallet : parseInt(args[0]);
+    const user = await getUser(message.author.id);
+    const amount = args[0] === 'all' ? user.wallet : parseInt(args[0]);
 
     if (isNaN(amount) || amount <= 0 || amount > user.wallet) {
       return message.reply('Invalid amount or insufficient wallet balance.');
     }
 
-    const newWallet = user.wallet - amount;
-    const newBank = user.bank + amount;
-    await updateUserEconomy(userId, newWallet, newBank);
+
+    user.wallet -= amount;
+    user.bank += amount;
+    await updateUser(message.author.id, user);
 
     const embed = new EmbedBuilder()
       .setColor('#9a46ca')
