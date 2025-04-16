fcd8e756ea5ef690ceeea3c3d24239d75d61ed65
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 12:11:50 +0300

    Update divorce.js

--- commands/divorce.js
+++ commands/divorce.js
@@ -1,4 +1,5 @@
-const { getUserFullProfile, updateUserProfile } = require('../database'); // Funções para atualizar o perfil no banco de dados
+const { getUserFullProfile, updateUserProfile } = require('../database');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'divorce',
@@ -6,16 +7,29 @@ module.exports = {
   async prefixExecute(message) {
     const profile = await getUserFullProfile(message.author.id);
     if (!profile.married_with) {
-      return message.reply('Você não está casado(a)!');
+      return message.reply('Você não está casado(a) com ninguém.');
     }
 
+    const spouseName = profile.married_with;
 
-    const spouse = profile.married_with;
+    // Encontrar o usuário pelo nome salvo (se possível)
+    const spouseUser = message.guild.members.cache.find(
+      member => member.user.username === spouseName
+    );
 
-
+    // Limpa o casamento nos dois perfis
     await updateUserProfile(message.author.id, { married_with: null });
-    await updateUserProfile(spouse.id, { married_with: null });
 
-    message.channel.send(`${message.author.username} e ${spouse} se divorciaram.`);
+    if (spouseUser) {
+      await updateUserProfile(spouseUser.user.id, { married_with: null });
+    }
+
+    const divorceEmbed = new EmbedBuilder()
+      .setTitle('Divórcio realizado')
+      .setDescription(`${message.author.username} se divorciou de ${spouseName}.`)
+      .setColor('#ff0000')
+      .setTimestamp();
+
+    message.channel.send({ embeds: [divorceEmbed] });
   }
 };
