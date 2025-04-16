5d65a41c974f63a374943ba8bd01dce010c428a9
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:18:16 +0300

    Create pay.js

---
+++ commands/pay.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder } = require('discord.js');
+const { getUser, updateUser } = require('../database');
+
+module.exports = {
+  name: 'pay',
+  description: 'Pay another user Synths.',
+  async prefixExecute(message, args) {
+    const member = message.mentions.members.first();
+    const amount = parseInt(args[1]);
+
+    if (!member || member.user.bot || member.id === message.author.id) {
+      return message.reply('Mention a valid user to pay.');
+    }
+
+    if (isNaN(amount) || amount <= 0) {
+      return message.reply('Enter a valid amount.');
+    }
+
+    const sender = getUser(message.author.id);
+    const receiver = getUser(member.id);
+
+    if (sender.wallet < amount) {
+      return message.reply('You do not have enough funds.');
+    }
+
+    sender.wallet -= amount;
+    receiver.wallet += amount;
+    updateUser(message.author.id, sender);
+    updateUser(member.id, receiver);
+
+    const embed = new EmbedBuilder()
+      .setColor('#9a46ca')
+      .setTitle('Payment Sent')
+      .setDescription(`You paid ${member.user.tag} ${amount} Synths.`);
+
+    message.reply({ embeds: [embed] });
+  }
+};
