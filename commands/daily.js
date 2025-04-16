cc3b7959a04ceb7bb5a2a64a9008ced7ff6ab0c2
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-16 09:33:30 +0300

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,6 +1,8 @@
 const { EmbedBuilder } = require('discord.js');
 const { getUser, updateUser } = require('../database');
 
+const COOLDOWN_TIME = 86400000; // 24 hours in milliseconds
+
 module.exports = {
   name: 'daily',
   description: 'Claim your daily reward.',
@@ -8,15 +10,19 @@ module.exports = {
     const user = getUser(message.author.id);
     const now = Date.now();
 
-    if (now - user.lastDaily < 86400000) {
-      const timeLeft = 86400000 - (now - user.lastDaily);
-      return message.reply(`You've already claimed your daily reward. Try again in ${Math.ceil(timeLeft / 3600000)}h.`);
+
+    if (now - user.lastDaily < COOLDOWN_TIME) {
+      const timeLeft = COOLDOWN_TIME - (now - user.lastDaily);
+      const hoursLeft = Math.ceil(timeLeft / 3600000);
+      return message.reply(`You've already claimed your daily reward. Try again in ${hoursLeft} hour(s).`);
     }
+
 
     const amount = 500;
     user.wallet += amount;
     user.lastDaily = now;
     updateUser(message.author.id, user);
+
 
     const embed = new EmbedBuilder()
       .setColor('#9a46ca')
