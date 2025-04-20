68aea7b6304bfd25c9e369fa0b825eed9bd8b98a
smallworldi <viktoriakuznetsov@mail.ru>
2025-04-20 12:19:05 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,61 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ban')
-    .setDescription('Забанить пользователя')
-    .addUserOption(option =>
-      option.setName('пользователь')
-        .setDescription('Укажите пользователя для бана')
-        .setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('причина')
-        .setDescription('Причина бана')
-        .setRequired(false)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
-
   name: 'ban',
   description: 'Забанить пользователя',
-
-  async execute(interaction) {
-    const member = interaction.options.getMember('пользователь');
-    const reason = interaction.options.getString('причина') || 'Не указано';
-
-    if (!member.bannable) {
-      return interaction.reply({ content: 'Я не могу забанить этого пользователя.', ephemeral: true });
-    }
-
-    if (member.roles.highest.position >= interaction.member.roles.highest.position) {
-      return interaction.reply({ content: 'Вы не можете забанить пользователя с равной или более высокой ролью.', ephemeral: true });
-    }
-
-    try {
-      await member.ban({ reason });
-
-      const embed = new EmbedBuilder()
-        .setTitle('Пользователь забанен')
-        .setColor('#000000')
-        .addFields(
-          { name: 'Пользователь:', value: `${member.user.tag} \`${member.user.id}\``, inline: false },
-          { name: 'Модератор:', value: `${interaction.user.tag} \`${interaction.user.id}\``, inline: false },
-          { name: 'Причина:', value: reason, inline: false }
-        )
-        .setTimestamp();
-
-      await interaction.reply({ embeds: [embed] });
-
-      const logChannel = interaction.guild.channels.cache.get('1344024905250771005');
-      if (logChannel) {
-        await logChannel.send({ embeds: [embed] });
-      }
-    } catch (err) {
-      console.error(err);
-      return interaction.reply({ content: 'Произошла ошибка при попытке забанить пользователя.', ephemeral: true });
-    }
-  },
-
   async prefixExecute(message, args) {
     if (!message.member.permissions.has(PermissionFlagsBits.BanMembers)) {
       return message.reply('У вас нет прав для использования этой команды.');
@@ -74,8 +21,7 @@ module.exports = {
       return message.reply('Я не могу забанить этого пользователя.');
     }
 
-    const reason = args.slice(1).join(' ') || 'Не указано';
-
+    const reason = args.slice(1).join(' ') || 'Не указано'; 
     try {
       await member.ban({ reason });
 
@@ -90,6 +36,7 @@ module.exports = {
         .setTimestamp();
 
       await message.channel.send({ embeds: [embed] });
+
 
       const logChannel = message.guild.channels.cache.get('1344024905250771005');
       if (logChannel) {
