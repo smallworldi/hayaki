6dd23819ca3720ff10d378d5fcbd08a192d00f0c
viktoria kuznetsova <viktoriakuznetsov@mail.ru>
2025-05-11 20:29:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ client.on(Events.InteractionCreate, async interaction => {
   } else if (interaction.customId === 'create_voice') {
     const member = await interaction.guild.members.fetch(interaction.user.id);
     if (member.voice.channelId !== '1363579823775154299') {
-      return interaction.reply({ content: 'Você precisa entrar no canal "➕・Criar Call" primeiro!', ephemeral: true });
+      return interaction.reply({ content: 'You need to join the "➕・Create Call" channel first!', ephemeral: true });
     }
 
     await interaction.reply('Please enter a name for your voice channel:');
